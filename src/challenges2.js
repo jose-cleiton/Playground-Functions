4809d77383f08c37f6a8d36a10a337dacf4e9ad9
Cleiton <cleitons835@gmail.com>
2021-12-03 12:48:57 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -26,9 +26,62 @@ techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Lucas')
 
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+ /* 
+  Essa função peguei do blog
+   https://wbruno.com.br/expressao-regular/mascara-campo-de-telefone-em-javascript-com-regex-nono-digito-telefones-sao-paulo/ 
+  */
+function generatePhoneNumber(phoneNumber) {
+      function mtel(v){
+          v=v.replace(/\D/g,"");             //Remove tudo o que não é dígito
+          v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
+          v=v.replace(/(\d)(\d{4})$/,"$1-$2");    //Coloca hífen entre o quarto e o quinto dígitos
+          return v;
+      }
+      /*--------------------------------------------------------*/
+      /* funçao baseada no artigo 
+      https://www.ti-enxame.com/pt/javascript/como-contar-o-valor-duplicado-em-uma-matriz-no-javascript/1041477557/ 
+      */
+      function count(array_elements) {
+    
+        let  array_elementsOrdenado = Array.from(array_elements)
+        array_elementsOrdenado.sort();
+    
+        var current = null;
+        var cnt = 0;
+        for (let i in array_elementsOrdenado) {
+            if (array_elementsOrdenado[i] != current) {
+                if (cnt >= 3) {
+                   return true;
+                }
+                
+                current = array_elementsOrdenado[i];
+                cnt = 1;
+            } else {
+                cnt++;
+            }
+        }
+       
+    
+    }
+/* ----------------------------------------------------------------*/
+
+
+
+  if (phoneNumber.length===11) {
+     for (let i in phoneNumber ) {
+         if (count(phoneNumber)) {
+           return "não é possível gerar um número de telefone com esses valores";
+          } else  if (phoneNumber[i]<0 || phoneNumber[i]>9){
+            return ("não é possível gerar um número de telefone com esses valores")
+          }
+      }        
+          
+  }else {
+      return ("Array com tamanho incorreto.")
+    }
+      return mtel(phoneNumber.toString());
 }
+
 
 // Desafio 12
 function triangleCheck() {
